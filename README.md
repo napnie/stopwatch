4b5d71d83e19fb3770d9f7c79081fea4fd442cc9
Nitith Chayakul <c.nitith@gmail.com>
2017-01-28 13:52:42 +0700

    Initial checkin

---
+++ README.md
@@ -0,0 +1,15 @@
+# Stopwatch tasks by Nitith Chayakul
+I ran the tasks on my computer, and got
+these results:
+
+|Task                                                    |         Time|
+|:-------------------------------------------------------|------------:|
+|Append 50,000 chars to String                           | 0.755086 sec|
+|Append 100,000 chars to String                          | 2.416397 sec|
+|Append 100,000 chars to StringBuilder                   | 0.002534 sec|
+|Sum array of double primitives                          | 1.428403 sec|
+|Sum array of Double object                              | 5.015529 sec|
+|Sum array of BigDecimal                                 | 9.867771 sec|
+
+## Explanation of Results
+
---
+++ src/stopwatch/Stopwatch.java
@@ -0,0 +1,52 @@
+package stopwatch;
+
+/**
+ * Stopwatch that measure elapsed time between start time and stop time
+ * or until the present time.
+ * @author Nitith Chayakul
+ * @version 1.0
+ * @since Jan 28, 2017
+ */
+public class Stopwatch {
+	/** time that stopwatch is start, in nanoseconds. */
+	private long start;
+	/** time that stopwatch is stop, in nanoseonds. */
+	private long stop;
+	/** elapsed time between start time and stop time, in seconds. */
+	private double duration;
+	/** stopwatch's status, true if it is running. */
+	private boolean isRunning=false;
+	/** constatnt for converting nanoseconds to seconds. */
+	private final double NANOSECOND = 1.0E-9;
+
+	/** Start the stopwatch if it is not running. */
+	void start(){
+		if (!isRunning){
+			start = System.nanoTime();
+			isRunning = true;
+		}
+	}
+	
+	/** Stop the stopwatch if it is running. */
+	void stop(){
+		if (isRunning){
+			stop = System.nanoTime();
+			isRunning = false;
+			duration = (stop-start)*NANOSECOND;
+		}
+	}
+
+	/** @return true if Stopwatch is running */
+	boolean isRunning(){
+		return isRunning;
+	}
+
+	/** @return elapsed time between start time and stop time or present time if it is running, in senconds with decimal. */
+	double getElapsed(){
+		if (isRunning){
+			return (System.nanoTime()-start)*NANOSECOND;
+		}else{
+			return duration;
+		}
+	}
+}
