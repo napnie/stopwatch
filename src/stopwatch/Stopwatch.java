809b16e0de7d6121e26c8d94361cedfb4d337016
Nitith Chayakul <c.nitith@gmail.com>
2017-03-30 18:07:53 +0700

    make Stopwatch State machine

--- src/stopwatch/Stopwatch.java
+++ src/stopwatch/Stopwatch.java
@@ -10,42 +10,109 @@ package stopwatch;
 public class Stopwatch {
 	/** time that stopwatch is start, in nanoseconds. */
 	private long start;
-	/** time that stopwatch is stop, in nanoseonds. */
+	/** time that stopwatch is stop, in nanoseconds. */
 	private long stop;
 	/** elapsed time between start time and stop time, in seconds. */
 	private double duration;
-	/** stopwatch's status, true if it is running. */
-	private boolean isRunning=false;
-	/** constatnt for converting nanoseconds to seconds. */
+	/** Constant for converting nanoseconds to seconds. */
 	private final double NANOSECOND = 1.0E-9;
+	
+	private StopwatchState state;
+	private final StopwatchState RUNNING = new RunningState();
+	private final StopwatchState STOPPED = new StoppedState();
 
-	/** Start the stopwatch if it is not running. */
-	public void start() {
-		if (!isRunning) {
-			start = System.nanoTime();
-			isRunning = true;
-		}
+	public Stopwatch() {
+		setState( STOPPED );
 	}
 	
-	/** Stop the stopwatch if it is running. */
+	/**
+	 * @see StopwatchState#start()
+	 */
+	public void start() {
+		state.start();
+	}
+	
+	/**
+	 * @see StopwatchState#stop()
+	 */
 	public void stop() {
-		if (isRunning) {
-			stop = System.nanoTime();
-			isRunning = false;
-			duration = (stop-start)*NANOSECOND;
-		}
+		state.stop();
 	}
 
 	/** @return true if Stopwatch is running */
 	public boolean isRunning() {
-		return isRunning;
+		return state == RUNNING;
 	}
 
-	/** @return elapsed time between start time and stop time or present time if it is running, in senconds with decimal. */
+	/**
+	 * @see StopwatchState#getElapsed()
+	 * @return
+	 */
 	public double getElapsed(){
-		if (isRunning) {
+		return state.getElapsed();
+	}
+	
+	private void setState( StopwatchState state ) {
+		this.state = state;
+	}
+	
+	interface StopwatchState {
+
+		/** Start the stopwatch if it is not running. */
+		public void start() ;
+		
+		/** Stop the stopwatch if it is running. */
+		public void stop() ;
+
+		/** @return elapsed time between start time and stop time or present time if it is running, in senconds with decimal. */
+		public double getElapsed() ;
+	}
+	
+	public class RunningState implements StopwatchState {
+		
+		/**
+		 * @see StopwatchState#start()
+		 */
+		public void start() {
+			// nothing to do
+		}
+		
+		/**
+		 * @see StopwatchState#stop()
+		 */
+		public void stop() {
+			stop = System.nanoTime();
+			duration = (stop-start)*NANOSECOND;
+			setState( STOPPED );
+		}
+
+		/**
+		 * @see StopwatchState#getElapsed()
+		 */
+		public double getElapsed(){
 			return (System.nanoTime()-start)*NANOSECOND;
-		} else {
+		}
+	}
+	
+	public class StoppedState implements StopwatchState {
+		
+		/** @see StopwatchState#start() */
+		public void start() {
+			start = System.nanoTime();
+			setState( RUNNING );
+		}
+		
+		/**
+		 * @see StopwatchState#stop()
+		 */
+		public void stop() {
+			// nothing to do
+		}
+
+		/**
+		 * @see StopwatchState#getElapsed()
+		 */
+		public double getElapsed(){
 			return duration;
 		}
 	}
